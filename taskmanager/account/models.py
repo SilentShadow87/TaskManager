2f54960e55a59e721b6fe07da2e2aa9830383723
vladimir <vlada@way2cu.com>
2020-11-12 16:25:35 +0100

    Account: Define ProfileModel model.

--- taskmanager/account/models.py
+++ taskmanager/account/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
-# Create your models here.
+
+UserModel = get_user_model()
+
+class ProfileModel(models.Model):
+    user = models.OneToOneField(UserModel, related_name='profile', on_delete=models.CASCADE)
+    is_verified = models.BooleanField(default=False)
+    is_premium_member = models.BooleanField(default=False)
+
