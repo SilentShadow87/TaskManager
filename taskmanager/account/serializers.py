89d38611dd3f2fcfd7ae6420dd0c34d859abbefc
vladimir <vlada@way2cu.com>
2020-11-12 16:28:33 +0100

    Account: Make serializer for ProfileModel.

--- taskmanager/account/serializers.py
+++ taskmanager/account/serializers.py
@@ -1,15 +1,31 @@
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
+from rest_framework.validators import UniqueValidator
+from .models import ProfileModel
 
 
 # get default user model
 UserModel = get_user_model()
 
 
+class ProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ProfileModel
+        fields = ['is_verified', 'is_premium_member']
+
+
 class UserSerializer(serializers.ModelSerializer):
+    profile = ProfileSerializer(read_only=True)
+    email = serializers.EmailField(required=True,
+                validators=[UniqueValidator(
+                    queryset=UserModel.objects.all(),
+                    message='Email address already in use.')
+                    ]
+                )
+
     class Meta:
         model = UserModel
-        fields = ['username', 'email', 'password']
+        fields = ['username', 'email', 'password', 'profile']
         extra_kwargs = {
                 'password': {'write_only': True}
                 }
@@ -17,8 +33,9 @@ class UserSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         password = validated_data.pop('password')
         user = UserModel(**validated_data)
-
+        user.is_active = False
         user.set_password(password)
         user.save()
 
+        ProfileModel.objects.create(user=user)
         return user
