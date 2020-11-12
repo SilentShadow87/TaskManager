ec9541870d725bc22f5309b391faf0229f1a0818
vladimir <vlada@way2cu.com>
2020-11-12 16:54:38 +0100

    Settings: Set default pagination.

--- taskmanager/account/views.py
+++ taskmanager/account/views.py
@@ -5,15 +5,19 @@ from rest_framework.permissions import AllowAny, IsAuthenticated
 
 from .serializers import UserSerializer
 
+
+# get default user model
 UserModel = get_user_model()
 
 class UserListView(ListAPIView):
-    permission_classes = [IsAuthenticated]
+    """Retrieve user list."""
+    permission_classes = [AllowAny]
     serializer_class = UserSerializer
     queryset = UserModel.objects.all()
 
 
 class UserCreateView(CreateAPIView):
+    """Reister new user."""
     permission_classes = [AllowAny]
     serializer_class = UserSerializer
     queryset = UserModel.objects.all()
