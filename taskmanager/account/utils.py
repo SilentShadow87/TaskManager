7ed14ec51c069fd33b8f070ff1b811f0e45c7c43
Vlada <vlada@way2cu.com>
2021-08-10 23:00:24 +0200

    Acount: Utils: Unite methods for sending mail with one time token/

--- taskmanager/account/utils.py
+++ taskmanager/account/utils.py
@@ -1,24 +1,29 @@
+from django.conf import settings
 from django.core.mail import send_mail
+from django.contrib.auth import get_user_model
+from django.contrib.auth.tokens import PasswordResetTokenGenerator
 from django.contrib.sites.shortcuts import get_current_site
 from django.template.loader import render_to_string
-from django.utils.encoding import force_bytes
-from django.utils.http import urlsafe_base64_encode
+from django.utils.encoding import force_bytes, force_text
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
 
 from .tokens import account_activation_token
 
 
-def send_confirmation_email(request, user):
+def send_one_time_token_mail(request, user, token, template, subject):
+    """Send an email to the user with one-time token link."""
     current_site = get_current_site(request)
 
-    subject = 'Email confirmation'
+    # prepare email subject and message
+    subject = subject
     message = render_to_string(
-            'account/activation_email.html', 
+            template,
             {
                 'request': request,
                 'user': user,
                 'domain': current_site.domain,
                 'uid': urlsafe_base64_encode(force_bytes(user.pk)),
-                'token': account_activation_token.make_token(user),
+                'token': token
             })
 
-    send_mail(subject, message, 'fejsov87@gmail.com', [user.email,])
+    send_mail(subject, message, settings.DEFAULT_FROM_EMAIL, [user.email,])
